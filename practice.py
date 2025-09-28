12579e0476625018a2989b84b4e5c2af96bf93fe
Ari <sariyanzero@gmail.com>
2025-09-28 13:51:54 -0400

    Initial

---
+++ practice.py
@@ -0,0 +1,38 @@
+############################# PRACTICE #############################
+# Q1: Prameter of rectangle
+a = 10
+b = 20
+#
+c = a + b
+d = c * 2
+print (d)
+# --------------------------------
+# Q2: Prameter of circle
+r = 5
+PI = 3.14
+#
+f = r * PI **2
+print(f)
+# --------------------------------
+# Q3: More geometry (Not Done)
+# Square
+# Triangle
+# Sphere
+# Mass
+# --------------------------------
+# Q4: Add limits (Not Done)
+a = 85
+if a > 80:
+    print("HELLL YAHHH")    # Indentation
+    print("Good Job")
+    print("You are amazing!")
+elif a > 70:
+    print("Woow")
+
+elif a > 50:
+    print("Try harder next time")
+else:
+  print("You have to study all summer")
+# --------------------------------
+# Q5: Detect odd or even numbers (Not Done)
+
