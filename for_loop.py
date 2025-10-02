5ea7aa311ba149504f6b10a976a041edbaec333e
Your Full Name <your.email@example.com>
2025-10-02 13:27:30 +0330

    Added loop

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+############################# FOR LOOP #############################
+# Loops are made to repeat a specific action or command
+#
+# 
+for list in [5, 2, 5, 3]: # "100" X wrong!          
+    z = list + 100 # Each are embedded one by one 
+    print(z)
+
+# A better and cleaner way
+list = [5, 2, 5, 3]
+for number in list: # "100" X wrong!          
+    z = number + 100 # Each are embedded one by one 
+    print(z)
+
+#
+
