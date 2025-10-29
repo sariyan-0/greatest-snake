9b6df450c14908499a2c4d5c9c5ed9445f810f02
Ari <sariyanzero@gmail.com>
2025-10-29 16:20:37 -0400

    Minor formatting fixes

--- variables.py
+++ variables.py
@@ -1,20 +1,20 @@
-#This is a comment!
+# This is a comment!
 
 cup = 25
 ali = 67
 google1 = 20
 IlovePrismGo100 = 100
-#three rules
-#  12burgers = 10       #cannot begin wit numbers   
-#  $money = 50          #No special characters
-#  vanilla cake = 30    #No Space
+# Three rules
+# 12burgers = 10       # Cannot begin wit numbers   
+# $money = 50          # No special characters
+# vanilla cake = 30    # No Space
 
 #-----------------------------------------
 
-#Standard Naming:                           Lower_snake_case
+# Standard Naming:                           Lower_snake_case
 apple_pie = 10
 iphone_15 = 1000
-#Scientific terms with their values         Upper_snake-case
+# Scientific terms with their values         Upper_snake-case
 GRAVITY = 9.8
 PI = 3.14159265359
 
