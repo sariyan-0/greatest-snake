89743724d0ddda535efc046deab282d402c10280
Ari <ari@powerosofzero.com>
2025-11-27 13:20:20 +0330

    Added a few examples

--- functions/function_practice.py
+++ functions/function_practice.py
@@ -8,7 +8,46 @@ def divisor(x):
             z.append(item)
     return z # <return> only works once
 
-a = divisor(12) 
-print(a)
+# Call
+# a = divisor(12) 
+# print(a)
+############################################################
+# Q2: Detect if x is a prime or not
 
+def is_prime(x):  
+    c = 0
+    for item in range(1, x + 1):
+        if x % item == 0:
+            c = c + 1  
+    if c == 2:
+        return "A prime number"
+    else:
+        return "Not a prime number"
 
+# Call       
+# a = is_prime(13)
+# print(a)
+############################################################
+# Q3: Find the smallest number in the list
+def minimun(lst_x):
+    mini = lst_x[0]
+    for item in lst_x:
+        if item < mini:
+            mini = item
+    return mini
+
+# Call
+# a = minimun(    [12, 18, 7, 65, 35, 3, 15, 4 ]  )
+# print (a) # 3
+############################################################
+# Q4: Find the biggest number in the list
+def maximum(lst_x):
+    maxi = lst_x[0]
+    for item in lst_x:
+        if item > maxi:
+            maxi = item
+    return maxi
+
+# Call
+a = maximum(    [12, 18, 7, 65, 35, 3, 15, 4 ]  )
+print (a) # 3
